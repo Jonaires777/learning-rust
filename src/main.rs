47ccb2586b6b88d4015ab1f3bc34c944d8df0fb4
Jonathan Aires <airesjonathan123@gmail.com>
2023-10-17 15:42:59 -0300

    Testcase: List

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,24 @@
-// This is how we comment in Rust
-use std::fmt::{self};
+use std::fmt;
 
-#[derive(Debug)]
-struct Person<'a> {
-    name: &'a str,
-    age: u8,
-}
+struct List(Vec<i32>);
 
-struct Structure(i32);
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
 
-#[derive(Debug)]
-struct MinMax(i64, i64);
+        write!(f, "[")?;
 
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: ", count)?;
+            write!(f, "{}", v)?;
+        }
+
+        write!(f, "]")
     }
 }
 
-impl fmt::Display for Structure {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl fmt::Display for Person<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Hi my name is {}, I'm {} years old", self.name, self.age)
-    }
-}
-
-#[derive(Debug)]
-struct Point2D{
-    x: f64,
-    y: f64,
-}
-
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
-
-fn main() {
-    let name = "Peter";
-    let age = 27;
-    let peter = Person {name, age};
-
-    let point = Point2D{x: 3.3, y: 7.2};
-
-    println!("Comaparing structures");
-    println!("{:#?}", peter);
-    println!("{}", peter);
-
-    println!("Comparing Points.");
-    println!("{}", point);
-    println!("{:?}", point)
+fn main(){
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
