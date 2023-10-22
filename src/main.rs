e8cb1aa9f1e3262377a65dabb75e82aa97c71675
Jonathan Aires <airesjonathan123@gmail.com>
2023-10-22 16:28:09 -0300

    Working with tuples

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use std::fmt;
 
-struct List(Vec<i32>);
+struct Matrix(f32,f32,f32,f32);
 
-impl fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let vec = &self.0;
-
-        write!(f, "[")?;
-
-        for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
-            write!(f, "{}: ", count)?;
-            write!(f, "{}", v)?;
-        }
-
-        write!(f, "]")
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "( {} {} )", &self.0, &self.1)?;
+        write!(f, "( {} {} )", &self.2, &self.3)
     }
 }
 
+fn transpose(matrix: Matrix) -> Matrix {
+    let mut transposed_matrix = matrix;
+
+    let aux = transposed_matrix.1;
+    transposed_matrix.1 = transposed_matrix.2;
+    transposed_matrix.2 = aux;
+
+    transposed_matrix
+}
+
 fn main(){
-    let v = List(vec![1, 2, 3]);
-    println!("{}", v);
+    let matrix = Matrix(1.1,1.2,2.1,2.2);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix))
 }
